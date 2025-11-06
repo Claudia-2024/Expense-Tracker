f0d34079f267677ad80f1a23d83d63713c75dcde
Claudia <claudiatindo95@gmail.com>
2025-11-06 22:23:37 +0100

    Initialisation of springboot project fro backend and react native project for frontend

---
+++ Backend/BudgItUp/src/main/java/org/g3_inge4_isi_en/budgitup/BudgItUpApplication.java
@@ -0,0 +1,13 @@
+package org.g3_inge4_isi_en.budgitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgItUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgItUpApplication.class, args);
+    }
+
+}
---
+++ Backend/BudgItUp/src/test/java/org/g3_inge4_isi_en/budgitup/BudgItUpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.g3_inge4_isi_en.budgitup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgItUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
